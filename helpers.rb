752783a7e924da6e57a21d7d9d3efb3629f088b9
Yoteichi <plonk@piano.email.ne.jp>
2015-01-16 19:24:23 +0900

    時刻表示ヘルパー関数

--- helpers.rb
+++ helpers.rb
@@ -61,5 +61,25 @@ class Pcgw
         ''
       end
     end
+
+    def render_date t
+      weekday = [*'日月火水木金土'.each_char]
+      delta = Time.now - t
+
+      case delta
+      when 0...1
+        "たった今"
+      when 1...(1.minute)
+        "#{delta.to_i}秒前"
+      when (1.minute)...(1.hour)
+        "#{(delta / 60).to_i}分前"
+      when (1.hour)...(24.hours)
+        "#{(delta / 3600).to_i}時間前"
+        # when (1.day)...Float::INFINITY
+      else
+        "#{t.year}/#{t.month}/#{t.day}(#{weekday[t.wday]}) #{t.hour}:#{t.min}:#{t.sec}"
+      end
+    end
+
   end
 end
