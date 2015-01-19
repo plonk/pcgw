cc151d0bb7d768c7bf1dca159aec1a7481618e50
Yoteichi <plonk@piano.email.ne.jp>
2015-01-19 11:07:03 +0900

    使っていないメソッドを削除

--- peercast.rb
+++ peercast.rb
@@ -1,21 +1,16 @@
 require 'jimson'
 
-class Peercast < Jimson::ClientHelper
+class Peercast
   def initialize(host, port)
-    super("http://#{host}:#{port}/api/1")
+    @helper = Jimson::ClientHelper.new("http://#{host}:#{port}/api/1")
   end
 
   def method_missing(*_args, &block)
     name, *args = _args
     if args.size == 1 and args[0].is_a? Hash
-      process_call(name, *args, &block)
+      @helper.process_call(name, *args, &block)
     else
-      process_call(name, args, &block)
+      @helper.process_call(name, args, &block)
     end
-  end
-
-  def has_channel?(name)
-    channels = process_call(:getChannels, [])
-    channels.any? { |ch| ch['info']['name'] == name }
   end
 end
