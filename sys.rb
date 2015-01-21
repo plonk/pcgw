72b2271aaec81a519bd6f6f8a9ebc3272079f515
Yoteichi <plonk@piano.email.ne.jp>
2015-01-21 15:06:55 +0900

    要らないファイルを消した

--- sys.rb
+++
@@ -1,6 +0,0 @@
-def used_ip4_ports
-  buf = `/bin/netstat -lnt`
-  buf.each_line.drop(2).map(&:split).select { |prot, | p(prot) == "tcp" }.map { |_, _, _, x, | x.split(':').last.to_i }
-end
-
-p used_ip4_ports
