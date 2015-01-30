2d946f9143e391d8be1bcff88163e373b5673f0a
Yoteichi <plonk@piano.email.ne.jp>
2015-01-30 11:10:43 +0900

    Logging モジュールの使い方の説明をコメントに書いた

--- logging.rb
+++ logging.rb
@@ -1,5 +1,23 @@
 require 'logger'
 
+=begin
+ログ取りモジュール
+
+初期化するには Logging.logger に Logger オブジェクトを代入します。
+
+    Logging.logger = Logger.new(STDOUT)
+
+クラスに Logging をインクルードすると、そのクラスのメソッドで Logger オ
+ブジェクトを log の名前で使えるようになります。
+
+    class App
+      include Logging
+      def initialize
+        log.info('Application initialized')
+      end
+      ...
+
+=end
 module Logging
   class << self
     attr_accessor :logger
