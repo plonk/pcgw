889f721cb14dc5062f657e84d236fe93e974701b
Yoteichi <plonk@piano.email.ne.jp>
2015-02-06 20:59:50 +0900

    IndexTxtモジュールを helpers.rb に移した

--- routes/index_txt.rb
+++ routes/index_txt.rb
@@ -1,18 +1,4 @@
-module IndexTxt
-  def uptime_fmt(sec)
-    min = (sec % 3600) / 60
-    hour = sec / 3600
-    "%d:%02d" % [hour, min]
-  end
-  module_function :uptime_fmt
-
-  ESCAPE_TABLE = { '<' => '&lt;', '>' => '&gt;' }
-  def field_escape(str)
-    str.gsub(/[<>]/) { |ch| ESCAPE_TABLE[ch] }
-  end
-  module_function :field_escape
-end
-
+# -*- coding: utf-8 -*-
 class Pcgw
   # YP4G 互換の index.txt
   get '/index.txt' do
