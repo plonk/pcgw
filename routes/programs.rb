20dc3fda9f8af90c3eb0293c8cc0f3f64602d373
Yoteichi <plonk@piano.email.ne.jp>
2015-02-12 02:39:09 +0900

    配信履歴をページめくりして閲覧できるようにした

--- routes/programs.rb
+++ routes/programs.rb
@@ -1,7 +1,35 @@
+# 配信履歴。「番組情報」
+
 class Pcgw < Sinatra::Base
   get '/programs/?' do
-    programs = ChannelInfo.all.order(created_at: :desc)
-    slim :programs, locals: { programs: programs }
+    redirect to "/programs/before/#{Time.now.to_i}/pages/1"
+  end
+
+  get '/programs/before/:start/pages/:num' do
+    halt(400, 'time not specified') if params['start'].blank?
+
+    start = Time.at(params['start'].to_i)
+    num = params['num'].to_i
+
+    programs = ChannelInfo.where('created_at <= ?', start)
+      .order(created_at: :desc)
+
+    halt(404, 'data not found') if programs.empty?
+
+    pages = programs.each_slice(10).to_a
+
+    unless num.between?(1, pages.size)
+      halt(400, 'page number out of range')
+    end
+
+    page = pages[num - 1]
+    pagination = OpenStruct.new(first: 1, last: pages.size, current: num)
+
+    slim :programs, locals: {
+      programs: page,
+      pagination: pagination,
+      base_path: "/programs/before/#{params['start']}/pages"
+    }
   end
 
   delete '/programs/:id' do |id|
