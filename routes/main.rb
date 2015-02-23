5696ba3fe3495c82b449520702c9c5f8956abf71
Yoteichi <plonk@piano.email.ne.jp>
2015-02-23 13:55:09 +0900

    top と home を ERB から Slim にした

--- routes/main.rb
+++ routes/main.rb
@@ -3,16 +3,14 @@ class Pcgw < Sinatra::Base
   get '/' do
     @channels = Channel.all
     programs = ChannelInfo.all.order(created_at: :desc).limit(10)
-    erb :top, locals: {
-      recent_programs: programs
-    }
+    slim :top, locals: { recent_programs: programs }
   end
 
   # ホーム
   get '/home' do
     @channels = @user.channels
     programs = ChannelInfo.where(user: @user).order(created_at: :desc).limit(10)
-    erb :home, locals: { recent_programs: programs }
+    slim :home, locals: { recent_programs: programs }
   end
 
 end
