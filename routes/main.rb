fc8ef0e5d9f65b25d88b4fc38a81f5523d82d892
Yoteichi <plonk@piano.email.ne.jp>
2016-07-29 19:50:51 +0900

    Revert "右上のボタンで状況に応じて別の事をするようにした。トップページの放送履歴にも[続きを配信]ボタンを付け、[もっと見る]ボタンで月別履歴のページに行くかわりにテーブルを延長するようにした。"
    This reverts commit 4aa22d662b88f220451cfcb31b807e95ddefa725.

--- routes/main.rb
+++ routes/main.rb
@@ -1,13 +1,10 @@
 class Pcgw < Sinatra::Base
   # ルート。
   get '/' do
-    limit = params['limit'] ? params['limit'].to_i : 10
-    limit = [[10, limit].max, 100].min
-
     @channels = Channel.all
-    programs = ChannelInfo.all.order(created_at: :desc).limit(limit)
+    programs = ChannelInfo.all.order(created_at: :desc).limit(10)
     max_channels = Servent.total_capacity
-    slim :top, locals: { recent_programs: programs, max_channels: max_channels, current_limit: limit, maxed: limit == 100 }
+    slim :top, locals: { recent_programs: programs, max_channels: max_channels }
   end
 
   # ホーム
@@ -15,23 +12,6 @@ class Pcgw < Sinatra::Base
     @channels = @user.channels
     programs = ChannelInfo.where(user: @user).order(created_at: :desc).limit(10)
     slim :home, locals: { recent_programs: programs }
-  end
-
-
-  # チャンネル操作のショートカット
-  get '/channel' do
-    # チャンネルがまだない場合はチャンネル作成画面へ。
-    # チャンネルが1つある場合はチャンネル状態画面へ。
-    # チャンネルが複数ある場合はホーム画面へ。(チャンネル一覧があるため)
-
-    case @user.channels.count
-    when 0
-      redirect to '/create'
-    when 1
-      redirect to "/channels/#{@user.channels[0].id}"
-    else
-      redirect to '/home'
-    end
   end
 
 end
