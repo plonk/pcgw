e586d49a4828077727bd8a01d9e741969e5781ca
Yoteichi <plonk@piano.email.ne.jp>
2016-07-29 23:26:19 +0900

    ジャンル文字列の親切な編集機能を追加した。

--- models/genre.rb
+++ models/genre.rb
@@ -1,19 +1,21 @@
 # ジャンル文字列に追加された YP4G コマンドを解釈する
 class Genre
-  attr_reader :namespace, :minimum_bandwidth, :proper
+  attr_reader :namespace, :minimum_bandwidth, :proper, :no_statistics
 
   def initialize(genre_string)
     @genre_string = genre_string
 
-    if /^.p(?:([a-zA-Z0-9]*):)?(\??)(@*)(.*)$/ =~ genre_string
+    if /^.p(?:([a-zA-Z0-9]*):)?(\??)(@*)(\+?)(.*)$/ =~ genre_string
       @namespace = $1
       @hide_listener_count = ($2 == '?')
       @minimum_bandwidth = $3
-      @proper = $4
+      @no_statistics = ($4 == '+')
+      @proper = $5
     else
       @namespace = ''
       @hide_listener_count = false
       @minimum_bandwidth = ''
+      @no_statistics = false
       @proper = genre_string
     end
   end
