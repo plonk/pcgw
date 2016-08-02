b6b9c7ab5adb3dfeb14997cf29240a7be0ed488f
Yoteichi <plonk@piano.email.ne.jp>
2016-08-02 19:41:51 +0900

    Revert "ジャンル文字列の親切な編集機能を追加した。"
    This reverts commit e586d49a4828077727bd8a01d9e741969e5781ca.

--- models/genre.rb
+++ models/genre.rb
@@ -1,21 +1,19 @@
 # ジャンル文字列に追加された YP4G コマンドを解釈する
 class Genre
-  attr_reader :namespace, :minimum_bandwidth, :proper, :no_statistics
+  attr_reader :namespace, :minimum_bandwidth, :proper
 
   def initialize(genre_string)
     @genre_string = genre_string
 
-    if /^.p(?:([a-zA-Z0-9]*):)?(\??)(@*)(\+?)(.*)$/ =~ genre_string
+    if /^.p(?:([a-zA-Z0-9]*):)?(\??)(@*)(.*)$/ =~ genre_string
       @namespace = $1
       @hide_listener_count = ($2 == '?')
       @minimum_bandwidth = $3
-      @no_statistics = ($4 == '+')
-      @proper = $5
+      @proper = $4
     else
       @namespace = ''
       @hide_listener_count = false
       @minimum_bandwidth = ''
-      @no_statistics = false
       @proper = genre_string
     end
   end
