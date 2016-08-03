b241e2bb0daec7eccd80f56ae89ff021fe23b84f
Yoteichi <plonk@piano.email.ne.jp>
2016-08-04 01:13:41 +0900

    部品の自動リロードを実装した。

--- routes/main.rb
+++ routes/main.rb
@@ -7,12 +7,26 @@ class Pcgw < Sinatra::Base
     slim :top, locals: { recent_programs: programs, max_channels: max_channels }
   end
 
+  get '/includes/my_channels' do
+    slim :my_channels, locals: { channels: @user.channels }, layout: false
+  end
+
+  get '/includes/onair' do
+    erb :onair, locals: { max_channels: Servent.total_capacity, channels: Channel.all }, layout: false
+  end
+
+  get '/includes/my_history' do
+    programs = ChannelInfo.where(user: @user).where.not(terminated_at: nil).order(created_at: :desc).limit(10)
+    slim :my_history, locals: { recent_programs: programs }, layout: false
+  end
+
+  get '/includes/channel_switcher' do
+    slim :channel_switcher, locals: { channels: @user.channels }, layout: false
+  end
+
   # ホーム
   get '/home' do
-    @channels = @user.channels
-    @all_channels = Channel.all
-    programs = ChannelInfo.where(user: @user).order(created_at: :desc).limit(10)
-    slim :home, locals: { recent_programs: programs, max_channels: Servent.total_capacity }
+    slim :home
   end
 
 end
