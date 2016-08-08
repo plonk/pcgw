db87051e31d0279a9a107d3ef82a231727610a14
Yoteichi <plonk@piano.email.ne.jp>
2016-08-09 04:09:29 +0900

    アカウント設定画面をslimで書き直した。

--- routes/account.rb
+++ routes/account.rb
@@ -1,11 +1,11 @@
 class Pcgw < Sinatra::Base
   get '/account' do
-    erb :account
+    slim :account
   end
 
   post '/account' do
     @user.update!(params.slice('name'))
     @success_message = '変更を保存しました。'
-    erb :account
+    slim :account
   end
 end
