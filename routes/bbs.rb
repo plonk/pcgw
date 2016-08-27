b2b8b8576640cf2a64ace564e2d327521edf0838
Yoteichi <plonk@piano.email.ne.jp>
2016-08-28 04:00:02 +0900

    3秒以内にしたらばの反応が無い場合にはエラーにするようにした。

--- routes/bbs.rb
+++ routes/bbs.rb
@@ -9,48 +9,55 @@ class Pcgw < Sinatra::Base
     halt 400, 'Error: url key not provided' if params[:url].blank?
 
     url = params[:url]
-    case url
-    when %r{\Ahttp://jbbs\.shitaraba\.net/bbs/read\.cgi/(\w+)/(\d+)/(\d+)(:?|\/.*)\z}
-      category = $1
-      board_num = $2.to_i
-      thread_num = $3.to_i
+    begin
+      Timeout.timeout(3) do 
+        case url
+        when %r{\Ahttp://jbbs\.shitaraba\.net/bbs/read\.cgi/(\w+)/(\d+)/(\d+)(:?|\/.*)\z}
+          category = $1
+          board_num = $2.to_i
+          thread_num = $3.to_i
 
-      board = Bbs::Board.new(category, board_num)
-      settings = board.settings
-      if settings.has_key?("ERROR")
-        return json_response({ "status"        => "error",
-                               "error_message" => "そのような板はありません。" })
+          board = Bbs::Board.new(category, board_num)
+          settings = board.settings
+          if settings.has_key?("ERROR")
+            return json_response({ "status"        => "error",
+                                   "error_message" => "そのような板はありません。" })
+          end
+
+          thread = board.thread(thread_num)
+          if thread.nil?
+            return json_response({ "status"        => "error",
+                                   "error_message" => "そのようなスレはありません。" })
+          end
+          return json_response({ "status"       => "ok",
+                                 "type"         => "thread",
+                                 "title"        => settings['BBS_TITLE'],
+                                 "thread_title" => thread.title,
+                                 "last"         => thread.last,
+                                 "max"          => settings['BBS_THREAD_STOP'].to_i })
+        when %r{\Ahttp://jbbs\.shitaraba\.net/(\w+)/(\d+)/?\z}
+          category = $1
+          board_num = $2.to_i
+
+          board = Bbs::Board.new(category, board_num)
+          settings = board.settings
+          if settings.has_key?("ERROR")
+            return json_response({ "status"        => "error",
+                                   "error_message" => "そのような板はありません。" })
+          end
+
+          return json_response({ "status" => "ok",
+                                 "type"   => "board",
+                                 "title"  => settings['BBS_TITLE'],
+                                 "max"    => settings['BBS_THREAD_STOP'].to_i })
+        else
+          return json_response({ "status"        => "error",
+                                 "error_message" => "したらば掲示板のURLではありません。" })
+        end
       end
-
-      thread = board.thread(thread_num)
-      if thread.nil?
-        return json_response({ "status"        => "error",
-                               "error_message" => "そのようなスレはありません。" })
-      end
-      return json_response({ "status"       => "ok",
-                             "type"         => "thread",
-                             "title"        => settings['BBS_TITLE'],
-                             "thread_title" => thread.title,
-                             "last"         => thread.last,
-                             "max"          => settings['BBS_THREAD_STOP'].to_i })
-    when %r{\Ahttp://jbbs\.shitaraba\.net/(\w+)/(\d+)/?\z}
-      category = $1
-      board_num = $2.to_i
-
-      board = Bbs::Board.new(category, board_num)
-      settings = board.settings
-      if settings.has_key?("ERROR")
-        return json_response({ "status"        => "error",
-                               "error_message" => "そのような板はありません。" })
-      end
-
-      return json_response({ "status" => "ok",
-                             "type"   => "board",
-                             "title"  => settings['BBS_TITLE'],
-                             "max"    => settings['BBS_THREAD_STOP'].to_i })
-    else
+    rescue Timeout::Error
       return json_response({ "status"        => "error",
-                             "error_message" => "したらば掲示板のURLではありません。" })
+                             "error_message" => "情報を得ることができませんでした。" })
     end
   end
   
