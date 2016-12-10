e28859e7b681882ea712971581b6843f7db49c73
Yoteichi <plonk@piano.email.ne.jp>
2016-12-10 22:25:40 +0900

    まだ終了していない配信についてダイジェストが生成できるようになれー

--- models/digest.rb
+++ models/digest.rb
@@ -9,7 +9,12 @@ class ProgramDigest
     screen_shots = program.screen_shots
                    .order(created_at: :asc)
                    .to_a
-    ranges = [*screen_shots.map { |s| s.created_at.localtime }, program.terminated_at.localtime + 5.minutes].each_cons(2).map { |t1, t2| t1...t2 }
+    if program.terminated_at
+      termination_time = program.terminated_at.localtime + 5.minutes
+    else
+      termination_time = Time.now.localtime
+    end
+    ranges = [*screen_shots.map { |s| s.created_at.localtime }, termination_time].each_cons(2).map { |t1, t2| t1...t2 }
 
     @groups = ranges.map.with_index { |range, i|
       ps = posts.select { |post| !post.deleted? && range.cover?(post.date) }
