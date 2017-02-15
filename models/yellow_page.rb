022d069282b9b7fcc76ed58390ef0fa58f4554e4
Yoteichi <plonk@piano.email.ne.jp>
2017-02-16 06:42:29 +0900

    テスト用のYPを追加。

--- models/yellow_page.rb
+++ models/yellow_page.rb
@@ -7,39 +7,40 @@ class YellowPage
                        'http://bayonet.ddo.jp/sp/notice.html',
                        'http://bayonet.ddo.jp/sp/',
                        'http://bayonet.ddo.jp/sp/favicon.ico',
-                       'pcp://bayonet.ddo.jp:7146/'],
+                       'pcp://bayonet.ddo.jp:7146/',
+                       false],
                       ['TP',
                        'tp',
                        'http://temp.orz.hm/yp/rule.html',
                        'http://temp.orz.hm/yp/',
                        'http://temp.orz.hm/yp/favicon.ico',
-                       'pcp://temp.orz.hm/'],
-                     ]
-      if ENV['PCGW_ENV'] == 'development'
-        yellow_pages << ['TestP',
-                         'tp',
-                         '',
-                         'http://localhost/',
-                         'http://ie.pcgw.pgw.jp/favicon.ico',
-                         'pcp://localhost:7146/']
-      end
-      return yellow_pages.map do |name, prefix, terms, top, icon, pcp|
-        YellowPage.new('name'=>name, 'prefix'=>prefix, 'terms'=>terms, 'top'=>top, 'icon'=>icon, 'uri'=>pcp)
+                       'pcp://temp.orz.hm/',
+                       false],
+                      ['TestP',
+                       '',
+                       '',
+                       'http:/yp.pcgw.pgw.jp/',
+                       'http://ie.pcgw.pgw.jp/favicon.ico',
+                       'pcp://yp.pcgw.pgw.jp:7146/',
+                       true]]
+      return yellow_pages.map do |name, prefix, terms, top, icon, pcp, admin_only|
+        YellowPage.new('name'=>name, 'prefix'=>prefix, 'terms'=>terms, 'top'=>top, 'icon'=>icon, 'uri'=>pcp, 'admin_only'=>admin_only)
       end
     end
 
   end
 
   attr_reader :name
-  attr_reader :prefix, :terms, :top, :icon, :uri
+  attr_reader :prefix, :terms, :top, :icon, :uri, :admin_only
 
   def initialize(hash)
-    @name = hash['name']
-    @prefix = hash['prefix']
-    @terms = hash['terms']
-    @top = hash['top']
-    @icon = hash['icon']
-    @uri = hash['uri']
+    @name       = hash['name']
+    @prefix     = hash['prefix']
+    @terms      = hash['terms']
+    @top        = hash['top']
+    @icon       = hash['icon']
+    @uri        = hash['uri']
+    @admin_only = hash['admin_only']
   end
 
   def prefixed?(genre)
