85b70fd6ad4a9cbf4c7d59e8c2c3475a43cd8caf
Yoteichi <plonk@piano.email.ne.jp>
2017-02-27 16:35:12 +0900

    お知らせの本文にHTMLタグを許すようにした。

--- models/notice.rb
+++ models/notice.rb
@@ -1,8 +1,10 @@
+require 'sanitize'
+
 class Notice < ActiveRecord::Base
   validates_length_of :title, in: 1..60
   validates_length_of :body, in: 1..1000
 
   def render_body
-    CGI::escapeHTML(body).gsub("\r\n", '<br>')
+    Sanitize.fragment(body.gsub("\r\n", '<br>'), Sanitize::Config::RELAXED)
   end
 end
