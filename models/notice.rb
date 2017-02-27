bb379eecdd63f5c9e4d9d54909c3ee5434d03a5c
Yoteichi <plonk@piano.email.ne.jp>
2017-02-27 16:30:20 +0900

    お知らせのタイトルやボディの長さ制限を緩めた。

--- models/notice.rb
+++ models/notice.rb
@@ -1,6 +1,6 @@
 class Notice < ActiveRecord::Base
-  validates_length_of :title, in: 1..30
-  validates_length_of :body, in: 1..500
+  validates_length_of :title, in: 1..60
+  validates_length_of :body, in: 1..1000
 
   def render_body
     CGI::escapeHTML(body).gsub("\r\n", '<br>')
