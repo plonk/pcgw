c6ce39f81abee636b13e4048f222f557ac574d39
Yoteichi <plonk@piano.email.ne.jp>
2017-05-24 02:33:29 +0900

    コメントを追加。

--- models/digest.rb
+++ models/digest.rb
@@ -9,17 +9,23 @@ class ProgramDigest
     screen_shots = program.screen_shots
                    .order(created_at: :asc)
                    .to_a
+
+    # 最後のスクリーンショットに対応する時間の範囲の終端
     if program.terminated_at
       termination_time = program.terminated_at.localtime + 5.minutes
     else
       termination_time = Time.now.localtime
     end
-    ranges = [*screen_shots.map { |s| s.created_at.localtime }, termination_time].each_cons(2).map { |t1, t2| t1...t2 }
+
+    # それぞれのスクリーンショットに対応する時間の範囲
+    ranges = [*screen_shots.map { |s| s.created_at.localtime }, termination_time]
+             .each_cons(2)
+             .map { |t1, t2| t1...t2 }
 
     @groups = ranges.map.with_index { |range, i|
       ps = posts.select { |post| !post.deleted? && range.cover?(post.date) }
       Group.new(screen_shots[i], ps)
     }
   end
-  
+
 end
