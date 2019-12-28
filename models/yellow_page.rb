6fec81a1b6d3aa934ffd49468d9c02194ee925ff
Yoteichi <plonk@piano.email.ne.jp>
2019-12-28 21:32:01 +0900

    YellowPage.all の計算をキャッシュするようにした。

--- models/yellow_page.rb
+++ models/yellow_page.rb
@@ -1,46 +1,43 @@
-# PeerCast Station が持っているイエローページの情報
+# 掲載先ルートサーバー。
 class YellowPage
   class << self
     def all
-      yellow_pages = [['SP',
-                       'sp',
-                       'http://bayonet.ddo.jp/sp/notice.html',
-                       'http://bayonet.ddo.jp/sp/',
-                       'http://bayonet.ddo.jp/sp/favicon.ico',
-                       'pcp://bayonet.ddo.jp:7146/',
-                       false],
-                      ['TP',
-                       'tp',
-                       'http://temp.orz.hm/yp/rule.html',
-                       'http://temp.orz.hm/yp/',
-                       'http://temp.orz.hm/yp/favicon.ico',
-                       'pcp://temp.orz.hm/',
-                       false],
-                      ['平成YP',
-                       '',
-                       'http://yp.pcgw.pgw.jp/kiyaku.html',
-                       'http://yp.pcgw.pgw.jp/',
-                       'http://yp.pcgw.pgw.jp/favicon.ico',
-                       'pcp://yp.pcgw.pgw.jp:7146/',
-                       false]]
-      return yellow_pages.map do |name, prefix, terms, top, icon, pcp, admin_only|
-        YellowPage.new('name'=>name, 'prefix'=>prefix, 'terms'=>terms, 'top'=>top, 'icon'=>icon, 'uri'=>pcp, 'admin_only'=>admin_only)
-      end
+      @all ||= [['SP',
+                 'sp',
+                 'http://bayonet.ddo.jp/sp/notice.html',
+                 'http://bayonet.ddo.jp/sp/',
+                 'http://bayonet.ddo.jp/sp/favicon.ico',
+                 'pcp://bayonet.ddo.jp:7146/',
+                 false],
+                ['TP',
+                 'tp',
+                 'http://temp.orz.hm/yp/rule.html',
+                 'http://temp.orz.hm/yp/',
+                 'http://temp.orz.hm/yp/favicon.ico',
+                 'pcp://temp.orz.hm/',
+                 false],
+                ['平成YP',
+                 '',
+                 'http://yp.pcgw.pgw.jp/kiyaku.html',
+                 'http://yp.pcgw.pgw.jp/',
+                 'http://yp.pcgw.pgw.jp/favicon.ico',
+                 'pcp://yp.pcgw.pgw.jp:7146/',
+                 false]].map { |args| YellowPage.new(*args) }
+      return @all
     end
 
   end
 
-  attr_reader :name
-  attr_reader :prefix, :terms, :top, :icon, :uri, :admin_only
+  attr_reader :name, :prefix, :terms, :top, :icon, :uri, :admin_only
 
-  def initialize(hash)
-    @name       = hash['name']
-    @prefix     = hash['prefix']
-    @terms      = hash['terms']
-    @top        = hash['top']
-    @icon       = hash['icon']
-    @uri        = hash['uri']
-    @admin_only = hash['admin_only']
+  def initialize(name, prefix, terms, top, icon, uri, admin_only)
+    @name       = name
+    @prefix     = prefix
+    @terms      = terms
+    @top        = top
+    @icon       = icon
+    @uri        = uri
+    @admin_only = admin_only
   end
 
   def prefixed?(genre)
