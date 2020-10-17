2348f994ed444e2aa3ad11d0355c4ba87dc99ae7
Yoteichi <plonk@piano.email.ne.jp>
2020-10-17 13:00:26 +0900

    リファクタリング。

--- init.rb
+++ init.rb
@@ -1,38 +1,25 @@
 # -*- coding: utf-8 -*-
+# アプリケーションの初期化処理
+
 require 'active_record'
 require 'yaml'
 
 # 設定ファイルのロード
-def load_config
-  YAML.load_file('config/config.yml')
-end
+CONFIG = YAML.load_file('config/config.yml')
 
 # 環境をチェックする
-def check_env
-  if ENV['PCGW_ENV'].blank?
-    puts "PCGW_ENV環境変数を設定して起動してください。(例: PCGW_ENV=production bin/pcgw)"
-    exit
-  end
-
-  if ENV['CONSUMER_KEY'].blank? or ENV['CONSUMER_SECRET'].blank?
-    puts "CONSUMER_KEY環境変数とCONSUMER_SECRET環境変数を設定して起動してください。"
-    exit
-  end
+if ENV['PCGW_ENV'].blank?
+  puts "PCGW_ENV環境変数を設定して起動してください。(例: PCGW_ENV=production bin/pcgw)"
+  exit
 end
 
-def pcgw_env
-  ENV['PCGW_ENV']
+if ENV['CONSUMER_KEY'].blank? or ENV['CONSUMER_SECRET'].blank?
+  puts "CONSUMER_KEY環境変数とCONSUMER_SECRET環境変数を設定して起動してください。"
+  exit
 end
 
-# DB に接続
-def db_connect
-  ActiveRecord::Base.establish_connection(CONFIG['db'][pcgw_env])
-end
+# DB に接続する
+ActiveRecord::Base.establish_connection(CONFIG['db'][ENV['PCGW_ENV']])
 
-# アプリケーションの初期化処理
-CONFIG = load_config
-check_env
-db_connect
-
-WM_MIRROR_HOSTNAME = CONFIG['wm_mirror'][pcgw_env]['hostname']
+WM_MIRROR_HOSTNAME = CONFIG['wm_mirror'][ENV['PCGW_ENV']]['hostname']
 PROXY_HOSTNAME = WM_MIRROR_HOSTNAME
