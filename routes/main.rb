5856595efcc3e6c272f2c2dac982b9a4361d8551
Yoteichi <plonk@piano.email.ne.jp>
2021-09-06 12:17:20 +0900

    ログインしなくてもパーシャルが取得できるようにパスを変えた。

--- routes/main.rb
+++ routes/main.rb
@@ -8,26 +8,21 @@ class Pcgw < Sinatra::Base
     slim :my_channels, locals: { channels: @user.channels }, layout: false
   end
 
-  get '/includes/onair' do
-    erb :includes_onair, locals: { channels: Channel.all }, layout: false
-  end
-
-  get '/includes/server_status' do
-    slim :includes_server_status, locals: { servents: Servent.enabled }, layout: false
-  end
-
-  # 現在配信中のチャンネル
-  get '/onair' do
-    slim :onair
-  end
-
   get '/includes/my_history' do
     programs = ChannelInfo.where(user: @user).where.not(terminated_at: nil).order(created_at: :desc).limit(10)
     slim :my_history, locals: { recent_programs: programs, user: @user }, layout: false
   end
 
+  # 現在配信中のチャンネルとサーバーの状態
   get '/stats' do
-    slim :stats, locals: { servents: Servent.enabled }
+    case params['partial']
+    when 'onair'
+      erb :includes_onair, locals: { channels: Channel.all }, layout: false
+    when 'server_status'
+      slim :includes_server_status, locals: { servents: Servent.enabled }, layout: false
+    else
+      slim :stats, locals: { servents: Servent.enabled }
+    end
   end
 
   # ホーム
